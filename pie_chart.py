2da13e621b5c1c6d920f76d1faa311ac6153a85c
drahmetacik <a.acik@icloud.com>
2024-11-25 20:41:53 +0000

    Add additional pie chart examples with various configurations

--- pie_chart.py
+++ pie_chart.py
@@ -8,3 +8,57 @@ explode = (0.1, 0, 0, 0)  # explode 1st slice
 plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
 plt.title('Pie Chart Example')
 plt.show()
+
+import matplotlib.pyplot as plt
+
+labels = ['A', 'B', 'C', 'D']
+sizes = [15, 30, 45, 10]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Pie Chart with Different Start Angle Example')
+plt.show()
+
+import matplotlib.pyplot as plt
+
+labels = ['A', 'B', 'C', 'D']
+sizes = [15, 30, 45, 10]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
+plt.title('Pie Chart with Percentage Labels Example')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+
+labels = ['A', 'B', 'C', 'D']
+sizes = [15, 30, 45, 10]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0.1, 0.1, 0.1)  # explode all slices
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.title('Pie Chart with Custom Explode Example')
+plt.show()
+
+import matplotlib.pyplot as plt
+
+labels = ['A', 'B', 'C', 'D']
+sizes = [15, 30, 45, 10]
+colors = ['red', 'blue', 'green', 'purple']
+
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.title('Pie Chart with Custom Colors Example')
+plt.show()
+
+import matplotlib.pyplot as plt
+
+labels = ['A', 'B', 'C', 'D']
+sizes = [15, 30, 45, 10]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.title('Pie Chart with Legend Example')
+plt.legend(labels, loc="best")
+plt.show()
+
